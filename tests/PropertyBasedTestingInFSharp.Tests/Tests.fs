7a009b1e061ab3abad9af90b8abfed500dbe5634
Jeremy Clough <jeremy@talesin.net>
2016-07-16 18:27:41 +1000

    initial commit

---
+++ tests/PropertyBasedTestingInFSharp.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module PropertyBasedTestingInFSharp.Tests
+
+open PropertyBasedTestingInFSharp
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)

054a8c411b76a50cfff466ec92866c1df7e2a636
Jeremy Clough <jeremy@talesin.net>
2016-08-10 19:31:29 +1000

    added in beginning of testing C# class

---
+++ src/CSharpDomain/SampleMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using StatsdClient;
+using log4net;
+using Stopwatch = System.Diagnostics.Stopwatch;
+
+namespace Examples
+{
+
+	public class SampleMiddleware : OwinMiddleware
+	{
+		private readonly IStatsd statsd;
+		private readonly ILog log;
+
+		public SampleMiddleware(OwinMiddleware next, IStatsd statsd, ILog log) : base(next)
+		{
+			this.statsd = statsd;
+			this.log = log;
+		}
+
+		public override async Task Invoke(IOwinContext context)
+		{
+			try
+			{
+				var stopwatch = Stopwatch.StartNew();
+
+				await Next.Invoke(context);
+
+				LogRequest(context.Request.Path.Value, stopwatch.ElapsedMilliseconds, context.Response.StatusCode, context.Request.Uri.ToString(), context.Request.QueryString);
+			}
+			catch (Exception e)
+			{
+				log.Error("Exception invoking next owin middleware component", e);
+			}
+		}
+
+
+		private void LogRequest(string metric, long responseTimeInMs, int statusCode, string url, QueryString queryString)
+		{
+			try
+			{
+				statsd.LogTiming($"{metric}.response_time", responseTimeInMs);
+				statsd.LogCount($"{metric}.status_code.{statusCode}", 1);
+			}
+			catch (Exception e)
+			{
+				log.Error("Exception logging to Statsd", e);
+			}
+
+			if (responseTimeInMs > 2000)
+			{
+				log.Warn($"Call took too long ({responseTimeInMs}ms): {statusCode} {url} {queryString} ");
+			}
+		}
+	}
+}
--- src/Eg/4-MoreRealistically.fs
+++ src/Eg/4-MoreRealistically.fs
@@ -1,10 +1,69 @@
 ﻿namespace Examples
 
 open System
+open System.Configuration
+open System.Web
+open System.Threading.Tasks
+
+open Microsoft.Owin
+open log4net
 
 open NUnit.Framework
-
 open FsCheck
 open FsCheck.NUnit
+open Foq
 
-// TODO example of testing a C# class with dependencies
+
+type Method = Method of string
+type Route = Route of string
+type SiteKey = SiteKey of string
+
+type StatsdMiddlewareGenerators () =
+    static let _null = Gen.constant null
+
+    static member OwinMiddleware () =
+        { new Arbitrary<OwinMiddleware>() with
+            override x.Generator = gen {
+                let mw = { new OwinMiddleware(null) with
+                                member this.Invoke (ctx) = Task.Run(fun () -> ())}
+
+                return mw }}
+
+    static member OwinContext () =
+        { new Arbitrary<IOwinContext>() with
+            override x.Generator = gen {
+
+                let sc = Mock<IReadableStringCollection>()
+                            .Create()
+
+                let rq = Mock<IOwinRequest>()
+                            .Setup(fun x -> <@ x.Query @>).Returns(sc)
+                            .Create()
+
+                let rs = Mock<IOwinResponse>()
+                            .Setup(fun x -> <@ x.StatusCode @>).Returns(1)
+                            .Create()
+
+                let! ctx = Mock<IOwinContext>()
+                            .Setup(fun x -> <@ x.Get<string>(any()) @>).Returns("")
+                            .Setup(fun x -> <@ x.Request @>).Returns(rq)
+                            .Setup(fun x -> <@ x.Response @>).Returns(rs)
+                            .Create()
+                            |> Gen.constant
+                            |> fun x -> Gen.oneof [ x; _null ]
+
+                return ctx }}
+
+module ``4 More Realistically`` =
+
+
+    [<Property(Arbitrary=[|typeof<StatsdMiddlewareGenerators>|])>]
+    let ``When invoked should result in logging of timing and count`` (owinMiddleware:OwinMiddleware) (owinContext:IOwinContext) =
+        let statsd = mock()
+        let log = mock()
+
+        let statsdmw = SampleMiddleware (owinMiddleware, statsd, log) 
+
+        statsdmw.Invoke (owinContext) |> Async.AwaitTask |> Async.RunSynchronously
+
+

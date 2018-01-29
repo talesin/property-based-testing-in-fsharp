e65be7fe53f20220dacc700af4944a9ce083d6b8
Jeremy Clough <jclough@seek.com.au>
2018-01-29 19:06:41 +1100

    Anyone notice the bug?

--- src/Eg/3b-UsingGenerators.fs
+++ src/Eg/3b-UsingGenerators.fs
@@ -24,7 +24,7 @@ module ``3 Using Generators`` =
            ver.Split '.'
            |> Array.map parseInt
            |> function
-           | [| x; y; z; |] -> (x, y, x)
+           | [| x; y; z; |] -> (x, y, x) // <-- anyone notice the bug?
            | _              -> (0, 0, 0)
 
        versions
